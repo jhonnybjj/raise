a63d432ea54dcd921749b8da2d99c8f3a93619ce
igormeireles <meireles.igor@yahoo.com.br>
2017-10-25 21:47:47 -0200

    indexb.html finalizado com sucesso - complementando

--- assets/index/index.js
+++ assets/index/index.js
@@ -1,23 +1,61 @@
-function abreLogin(){
-    document.getElementById('inicio').style.display = 'none';
-    document.getElementById('cadastro').style.display = 'none';
-    document.getElementById('login').style.display = 'block';
-}
-function fechaLogin(){
+function someDiv(){
     document.getElementById('login').style.display = 'none';
     document.getElementById('cadastro').style.display = 'none';
+    document.getElementById('lembrete').style.display = 'none';
+    document.getElementById('inicio').style.display = 'none';
+}
+function abreLogin(){
+    someDiv();
+    document.getElementById('login').style.display = 'block';
+
+}
+function fechaLogin(){
+    someDiv();
     document.getElementById('inicio').style.display = 'block';
 }
 function abreCadastro(){
-    document.getElementById('login').style.display = 'none';
-    document.getElementById('inicio').style.display = 'none';
+    someDiv();
     document.getElementById('cadastro').style.display = 'block';
 }
 function fechaCadastro(){
-    document.getElementById('login').style.display = 'none';
-    document.getElementById('cadastro').style.display = 'none';
+    someDiv();
     document.getElementById('inicio').style.display = 'block';
 }
 function abreLembrete(){
-
+    someDiv();
+    document.getElementById('lembrete').style.display = 'block';
+}
+function fechaLembrete(){
+    someDiv();
+    document.getElementById('inicio').style.display = 'block';
+}
+function executaCadastro(){
+    document.getElementById('loading').style.display = 'block';
+    setInterval(function(){
+        location='indexb.html';
+    }, 2000);
+}
+function executaCadastroFacebook(){
+    document.getElementById('loading').style.display = 'block';
+    setInterval(function(){
+        location='indexb.html';
+    }, 2000);
+}
+function executaLogin(){
+    document.getElementById('loading').style.display = 'block';
+    setInterval(function(){
+        location='indexb.html';
+    }, 2000);
+}
+function executaLoginFacebook(){
+    document.getElementById('loading').style.display = 'block';
+    setInterval(function(){
+        location='indexb.html';
+    }, 2000);
+}
+function executalembrete(){
+    document.getElementById('loading').style.display = 'block';
+    setInterval(function(){
+        location='indexb.html';
+    }, 2000);
 }
