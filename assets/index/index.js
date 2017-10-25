b8823830811cc103d344707832a18f677b408f8c
igormeireles <meireles.igor@yahoo.com.br>
2017-10-25 20:52:20 -0200

    criando a index layout correto

---
+++ assets/index/index.js
@@ -0,0 +1,23 @@
+function abreLogin(){
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('cadastro').style.display = 'none';
+    document.getElementById('login').style.display = 'block';
+}
+function fechaLogin(){
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('cadastro').style.display = 'none';
+    document.getElementById('inicio').style.display = 'block';
+}
+function abreCadastro(){
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('cadastro').style.display = 'block';
+}
+function fechaCadastro(){
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('cadastro').style.display = 'none';
+    document.getElementById('inicio').style.display = 'block';
+}
+function abreLembrete(){
+
+}
